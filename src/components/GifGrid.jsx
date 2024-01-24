69a29ea00577bd8f36b392215f724f68cc43e321
Lanavia <lorena.anavia@gmail.com>
2024-01-23 23:24:53 -0300

    first custom hook

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,35 +1,16 @@
-import { useEffect, useState } from 'react'
-import { getGifs } from '../helpers/GetGifs'
 import { GifItem } from './GifItem'
+import { useFetchGifs } from '../hooks/useFetchGifs'
 
 
 export const GifGrid = ({ category }) => {
-    const [images, setImages] = useState([])
+    const { images, isLoading } = useFetchGifs(category)
 
-    const getImages = async () => {
-        const newImages = await getGifs(category)
-        setImages(newImages)
-    }
-
-    useEffect(() => {
-        getImages()
-    }, [])
-
-
-
-    getGifs(category)
+    console.log({ images, isLoading })
     return (
         <>
             <h3>{category}</h3>
             <div className='card-grid'>
 
-                {/* opcion 1
-                images.map(({ id, title, url }) => (
-
-                    <GifItem key={id} title={title} url={url} />
-                ))
-
-                */}
 
                 { //opcion 2 esparcir todas las propiedades de la imagen con el operador spread ...
                     images.map((image) => (
--- src/helpers/GetGifs.jsx
+++ src/helpers/GetGifs.jsx
@@ -1,6 +1,5 @@
 export const getGifs = async (category) => {
 
-
     const url = `https://api.giphy.com/v1/gifs/search?api_key=BNBqn3cvy33JxVhwnhzVU434qGO3b0Sf&q=${category}&limit=3`
     const resp = await fetch(url)
     const { data } = await resp.json()
